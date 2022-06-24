8bc66a652ced5ddfbb38075ed919250902bd3336
Vladimir <leanca.vladimir@justcarpets.eu>
2022-06-25 00:28:40 +0300

    npx install next --ts (without scss/less)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
-
 const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  i18n,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import { appWithTranslation } from 'next-i18next';
-import '../../styles/globals.scss'
-import 'antd/dist/antd.css';
-
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
-
-export default appWithTranslation(MyApp)
